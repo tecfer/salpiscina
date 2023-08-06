3e5e92b0e7d8382f31dbda86060f6f60d383e37a
Carlos F.O <tecfer2020@gmail.com>
2023-08-06 11:45:27 +0200

    add copy and send to whatsapp

--- app/src/main/java/es/tecfer/salpiscina/MainActivity.java
+++ app/src/main/java/es/tecfer/salpiscina/MainActivity.java
@@ -1,5 +1,7 @@
 package es.tecfer.salpiscina;
 
+import static es.tecfer.salpiscina.R.id.b_calculate;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -7,13 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText ppmInput;
     private EditText metrosCuadradosInput;
-    private Button calculateButton;
+    private Button b_calculate, b_copy, b_whatsapp;
     private TextView resultTextView;
     private double rangoConcentracionMin = 4;
     private double rangoConcentracionMax = 7;
@@ -25,16 +32,25 @@ public class MainActivity extends AppCompatActivity {
 
         ppmInput = findViewById(R.id.ppm_input);
         metrosCuadradosInput = findViewById(R.id.metros_cuadrados_input);
-        calculateButton = findViewById(R.id.calculate_button);
+        b_calculate = findViewById(R.id.b_calculate);
+        b_copy = findViewById(R.id.b_copy);
+        b_whatsapp = findViewById(R.id.b_whatsapp);
+
         resultTextView = findViewById(R.id.result_text_view);
 
         metrosCuadradosInput.setText("50");
 
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        b_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculateSal();
             }
+        });b_copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { copyText();}
+        });b_whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { send_whatsapp(); }
         });
     }
 
@@ -44,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Ocultar el teclado numérico
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(calculateButton.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(b_calculate.getWindowToken(), 0);
 
         try {
             double ppmInicial = Double.parseDouble(ppmText);
@@ -64,11 +80,46 @@ public class MainActivity extends AppCompatActivity {
             int maxSacos = (int) Math.ceil(maxSal / 25);
 
             String result = "La sal a añadir es: " + minSal + " - " + maxSal + " kg\n" +
-                    " Sacos de 25 Kg: " + minSacos + " - " + maxSacos + " Sacos";
+                    "Sacos de 25 Kg: " + minSacos + " - " + maxSacos + " Sacos";
 
             resultTextView.setText(result);
         } catch (NumberFormatException e) {
             resultTextView.setText("Error: Los valores ingresados no son válidos. Asegúrate de ingresar números.");
+        }
+    }
+
+    private void copyText()
+    {
+        //Copia texto a portapapeles.
+
+        String text = String.valueOf(resultTextView.getText());
+        try {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("text",  text);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, getString(R.string.copy_msg), Toast.LENGTH_SHORT).show();
+        }catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, getString(R.string.copy_msg_error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void send_whatsapp()
+    {
+         // Envia el texto a WhatsApp
+
+        String text =   String.valueOf(resultTextView.getText());
+
+        try {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+
+        } catch (android.content.ActivityNotFoundException ex) {
+            ex.printStackTrace();
+            Toast.makeText(getBaseContext(),getString(R.string.error), Toast.LENGTH_SHORT).show();
         }
     }
 }
